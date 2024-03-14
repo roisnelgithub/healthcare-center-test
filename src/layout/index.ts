50e51e255053e0d2f0a5d06f0b29a0f6f931afc4
Roisnel <roy.adan.trenal@gmail.com>
2024-03-14 13:12:57 -0400

    refactor: :zap: refactor de about-page
    Se le añadió su contenido, el header y las descripciones de la compaía

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,1 +1,2 @@
 export { default as ShareLayout } from "./share-layout";
+export { default as HeaderPageLayout } from "./header-page-layout";
--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -1,7 +1,68 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { HeaderPageLayout } from "../../layout";
+import about from "../../assets/about.png";
+import { useContext } from "react";
+import { MQContext } from "../../context";
+import { CardDescription } from "../../components";
+
+const descriptions = [
+  {
+    id: 1,
+    title: "Moya Medical",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
+  },
+  {
+    id: 2,
+    title: "Nuestra Misión",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
+  },
+  {
+    id: 3,
+    title: "Por qué nosotros",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
+  },
+];
 
 const AboutPage = () => {
-  return <Box sx={{ height: "100vh" }}>About</Box>;
+  const { xs, sm, md, lg } = useContext(MQContext);
+  return (
+    <Box>
+      <HeaderPageLayout image={about}>
+        <Box sx={{ mb: "10vh" }}>
+          <Typography
+            variant={xs || sm ? "h3" : md || lg ? "h2" : "h1"}
+            fontWeight="bold"
+            sx={{ color: "white", textAlign: "center" }}
+          >
+            Nuestra compañía
+          </Typography>
+        </Box>
+      </HeaderPageLayout>
+      <br />
+      <hr />
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          my: "3rem",
+        }}
+      >
+        <Stack sx={{ maxWidth: "80%" }} alignItems={"center"} spacing={6}>
+          {descriptions.map((des) => (
+            <CardDescription
+              key={des.id}
+              title={des.title}
+              description={des.description}
+            />
+          ))}
+        </Stack>
+      </Box>
+    </Box>
+  );
 };
 
 export default AboutPage;
