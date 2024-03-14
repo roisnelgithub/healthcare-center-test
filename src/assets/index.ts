72a8ad3315f1e16cc7358c4b22d389b72272b682
Roisnel <roy.adan.trenal@gmail.com>
2024-03-14 16:18:09 -0400

    refactor: :recycle: adición del contenido de  los srvicios

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,6 +5,15 @@ import testTube from "./test-tube.png";
 import doctors from "./doctors-1.png";
 import flagSpain from "./flag-spain.png";
 import flagUsa from "./flag-usa.png";
+import services from "./services.png";
+import airplane from "./airplane.png";
+import fastAttention from "./fast-attention.png";
+import servicesMRO from "./services-mro.png";
+import people from "./people.png";
+import law from "./law.png";
+import medicalTest from "./medical-test.png";
+import recollection from "./recollection.png";
+import trunk from "./trunk.png";
 
 export {
   medicalBag,
@@ -14,4 +23,13 @@ export {
   doctors,
   flagSpain,
   flagUsa,
+  services,
+  airplane,
+  fastAttention,
+  servicesMRO,
+  people,
+  law,
+  medicalTest,
+  recollection,
+  trunk,
 };
--- src/pages/services-page/services-page.tsx
+++ src/pages/services-page/services-page.tsx
@@ -1,5 +1,130 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { HeaderPageLayout } from "../../layout";
+import { useContext } from "react";
+import { MQContext } from "../../context";
+import {
+  medicalBag,
+  medicalPlaster,
+  physicalExam,
+  testTube,
+  airplane,
+  services,
+  fastAttention,
+  servicesMRO,
+  people,
+  law,
+  medicalTest,
+  recollection as recollection,
+  trunk,
+} from "../../assets";
+import { CardGrid } from "../../components";
+
+const cards = [
+  {
+    id: 1,
+    title: "Atención de lesiones",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: medicalPlaster,
+  },
+  {
+    id: 2,
+    title: "Pruebas de droga y alcohol",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: testTube,
+  },
+  {
+    id: 3,
+    title: "Exámenes físicos",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: physicalExam,
+  },
+  {
+    id: 4,
+    title: "Medicina preventiva",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: medicalBag,
+  },
+  {
+    id: 5,
+    title: "Examen médico de inmigración",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: airplane,
+  },
+  {
+    id: 6,
+    title: "Atención rápida",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: fastAttention,
+  },
+  {
+    id: 7,
+    title: "Servicios MRO",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: servicesMRO,
+  },
+  {
+    id: 8,
+    title: "Programa de asistencia a empleados",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: people,
+  },
+  {
+    id: 9,
+    title: "Cumplimiento normativo",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: law,
+  },
+  {
+    id: 10,
+    title: "Recolección de muestras 24h",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: medicalTest,
+  },
+  {
+    id: 11,
+    title: "Red de sitios de recolección",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: recollection,
+  },
+  {
+    id: 12,
+    title: "Servicios DOT",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed diam",
+    icon: trunk,
+  },
+];
+
 const ServicesPage = () => {
-  return <div>Services</div>;
+  const { xs, sm, md, lg } = useContext(MQContext);
+  return (
+    <Box>
+      <HeaderPageLayout image={services}>
+        <Box sx={{ mb: "10vh" }}>
+          <Typography
+            variant={xs || sm ? "h3" : md || lg ? "h2" : "h1"}
+            fontWeight="bold"
+            sx={{ color: "white", textAlign: "center" }}
+          >
+            Nuestros Servicios
+          </Typography>
+        </Box>
+      </HeaderPageLayout>
+      <br />
+      <hr />
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          my: "3rem",
+        }}
+      >
+        <Stack sx={{ maxWidth: "80%" }} alignItems={"center"} spacing={6}>
+          <CardGrid cards={cards} />
+        </Stack>
+      </Box>
+    </Box>
+  );
 };
 
 export default ServicesPage;
