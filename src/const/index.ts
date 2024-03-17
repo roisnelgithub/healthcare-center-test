16f7b3d1552e8af75db30a4fc70ddd82779de011
Roisnel <roy.adan.trenal@gmail.com>
2024-03-16 23:41:33 -0400

    refactor: :art: movimiento de las descripciones de la compañía
    Se movieron las descripciones para una archivo de contantes

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,3 +1,4 @@
 export * from "./nav-bar-const";
 export * from "./services-const";
 export * from "./testimonials-const";
+export * from "./company-description-const";
--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -5,31 +5,7 @@ import { MQContext } from "../../context";
 import { CardDescription } from "../../components";
 import { useTranslation } from "react-i18next";
 import { aboutImg } from "../../assets";
-
-const descriptions = [
-  {
-    id: 1,
-    title: "Moya Medical",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
-    translateKey: "moyaMedical",
-  },
-  {
-    id: 2,
-    title: "Nuestra Misión",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
-    translateKey: "ourMission",
-  },
-  {
-    id: 3,
-    title: "Por qué nosotros",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, voluptatem atque natus quaerat dolore tempora qui ipsa aliquid provident sit, magnam iure fuga deleniti mollitia eos, eligendi iste tenetur libero. Id odit laborum accusantium suscipit enim corporis perferendis veniam quasi minima? Ipsum placeat perferendis excepturi optio dignissimos rem suscipit id tenetur praesentium. Temporibus dolorem natus iste cupiditate et aliquam corrupti eaque alias ea inventore, deserunt ab architecto sapiente dignissimos. Reiciendis, aliquid totam. Quo vitae quisquam incidunt magnam distinctio quod alias blanditiis tempore iusto accusamus. Similique velit sapiente sit suscipit hic?",
-    link: "/about/why-us",
-    translateKey: "whyUs",
-  },
-];
+import { descriptions } from "../../const";
 
 const AboutPage = () => {
   const { xs, sm, md, lg } = useContext(MQContext);
